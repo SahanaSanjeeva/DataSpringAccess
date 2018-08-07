5a9cee44430e02bef61f9c08d439a6b02589c864
SahanaSanjeeva <spsahana@gmail.com>
2018-08-07 14:52:35 -0500

    First commit

---
+++ DBAccess/src/main/java/com/dbbasicaccess/server/main/DbAccessApplication.java
@@ -0,0 +1,12 @@
+package com.dbbasicaccess.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbAccessApplication.class, args);
+	}
+}
